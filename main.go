75309d59b0836f5c2569ce5ef700e232309ad8e4
tMatSuZ <tmatsuzaki1@gmail.com>
2018-10-15 01:00:58 +0900

    retweet manに変更

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/dghubble/go-twitter/twitter"
-	"github.com/dghubble/oauth1"
 	"flag"
 	"log"
 	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 
 	"github.com/coreos/pkg/flagutil"
 )
@@ -20,11 +21,6 @@ func main() {
 	flags.Parse(os.Args[1:])
 	flagutil.SetFlagsFromEnv(flags, "TWITTER")
 
-	println(*consumerKey)
-	println(*consumerSecret)
-	println(*accessToken)
-	println(*accessSecret)
-
 	if *consumerKey == "" || *consumerSecret == "" || *accessToken == "" || *accessSecret == "" {
 		log.Fatal("Consumer key/secret and Access token/secret required")
 	}
@@ -35,9 +31,18 @@ func main() {
 
 	client := twitter.NewClient(httpClient)
 
-	tweets, _, err := client.Statuses.Update("test tweets", nil)
-	if err != nil {
-		fmt.Println(err.Error)
+	params := &twitter.StreamFilterParams{
+		Track:         []string{"golang", "Golang"},
+		StallWarnings: twitter.Bool(true),
 	}
-	fmt.Println(tweets.Text)
+	stream, _ := client.Streams.Filter(params)
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		fmt.Println(tweet.Text)
+		client.Statuses.Retweet(tweet.ID, nil)
+	}
+
+	for message := range stream.Messages {
+		demux.Handle(message)
+	}
 }
