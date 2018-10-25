75ff796621e608f13c79d5ee1a729e9c621af550
tMatSuZ <tmatsuzaki1@gmail.com>
2018-10-25 14:01:46 +0900

    お知らせ後のDB更新追加

--- goshirase.go
+++ goshirase.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/dghubble/go-twitter/twitter"
 )
@@ -16,8 +17,8 @@ func goshirase(client *twitter.Client) {
 	notices := []Notice{}
 
 	// 1日経過している場合、対象
-	db.Where("registered_time < (NOW() - INTERVAL 1 DAY)").Limit(900).Find(&notices)
-	// db.Where("registered_time < NOW()").Limit(900).Find(&notices) // debug
+	// db.Where("registered_time < (NOW() - INTERVAL 1 DAY)").Limit(900).Find(&notices)
+	db.Where("registered_time < NOW()").Limit(900).Find(&notices) // debug
 
 	for _, notice := range notices {
 		user, _, err := client.Users.Show(&twitter.UserShowParams{
@@ -31,17 +32,16 @@ func goshirase(client *twitter.Client) {
 		// 前回よりフォロワーが減っている場合お知らせしてあげる
 		diff := notice.FollowCount - user.FollowersCount
 		if diff > 0 {
-			message := strings.Replace(messageTmplate, "##UPTIME##", notice.RegisteredTime.Format("15時04分05秒"), -1)
+			message := strings.Replace(messageTmplate, "##UPTIME##", notice.RegisteredTime.Format("2006-01-02 15:04"), -1)
 			message = strings.Replace(message, "##FOL##", strconv.Itoa(diff), -1)
 			log.Println("@" + user.ScreenName + " " + message)
 			// client.Statuses.Update("@"+user.ScreenName+" "+message, nil)
 		}
-		// :TODO DB更新処理　フォロワーとdate
+		db.Model(&notice).Updates(Notice{FollowCount: user.FollowersCount, RegisteredTime: time.Now()})
 	}
-
 }
 
-var messageTmplate = `前日の##UPTIME##に比べ、フォロワーが##FOL##人減少しています。
+var messageTmplate = `##UPTIME##に比べ、フォロワーが##FOL##人減少しています。
 
 フォローしていただければ、このお知らせをずっと受け取れます。
 #goshirase
