5c9d81aa87983ff558c11ba33276488a4aa2b308
tMatSuZ <tmatsuzaki1@gmail.com>
2018-11-07 14:14:00 +0900

    main cli化、configure実装

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 	"os"
+	"sort"
 
-	"github.com/coreos/pkg/flagutil"
-	"github.com/dghubble/go-twitter/twitter"
-	"github.com/dghubble/oauth1"
 	"github.com/urfave/cli"
 )
 
@@ -17,28 +13,63 @@ func main() {
 	app.Name = "Goshirase"
 	app.Usage = "test usage"
 	app.Version = "0.0.1"
-	app.Run(os.Args)
 
-	flags := flag.NewFlagSet("user-auth", flag.ExitOnError)
-	consumerKey := flags.String("consumer-key", "", "Twitter Consumer Key")
-	consumerSecret := flags.String("consumer-secret", "", "Twitter Consumer Secret")
-	accessToken := flags.String("access-token", "", "Twitter Access Token")
-	accessSecret := flags.String("access-secret", "", "Twitter Access Secret")
-	flags.Parse(os.Args[1:])
-	flagutil.SetFlagsFromEnv(flags, "TWITTER")
-
-	if *consumerKey == "" || *consumerSecret == "" || *accessToken == "" || *accessSecret == "" {
-		log.Fatal("Consumer key/secret and Access token/secret required")
+	// flags
+	configName := ".goshirase/config"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "profile, p",
+			Value:       "config",
+			Usage:       "config file name",
+			Destination: &configName,
+		},
 	}
 
-	config := oauth1.NewConfig(*consumerKey, *consumerSecret)
-	token := oauth1.NewToken(*accessToken, *accessSecret)
-	httpClient := config.Client(oauth1.NoContext, token)
+	app.Commands = []cli.Command{
+		{
+			Name:    "configure",
+			Aliases: []string{"c"},
+			Usage:   "set config file",
+			Action: func(c *cli.Context) error {
+				err := registerConfig(configName)
+				return err
+			},
+		},
+		{
+			Name:    "add",
+			Aliases: []string{"g"},
+			Usage:   "add a task to the list",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+	}
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
 
-	client := twitter.NewClient(httpClient)
-	fmt.Println(client)
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	registerConfig("CONFIG_TEST")
+	// flags := flag.NewFlagSet("user-auth", flag.ExitOnError)
+	// consumerKey := flags.String("consumer-key", "", "Twitter Consumer Key")
+	// consumerSecret := flags.String("consumer-secret", "", "Twitter Consumer Secret")
+	// accessToken := flags.String("access-token", "", "Twitter Access Token")
+	// accessSecret := flags.String("access-secret", "", "Twitter Access Secret")
+	// flags.Parse(os.Args[1:])
+	// flagutil.SetFlagsFromEnv(flags, "TWITTER")
+
+	// if *consumerKey == "" || *consumerSecret == "" || *accessToken == "" || *accessSecret == "" {
+	// 	log.Fatal("Consumer key/secret and Access token/secret required")
+	// }
+
+	// config := oauth1.NewConfig(*consumerKey, *consumerSecret)
+	// token := oauth1.NewToken(*accessToken, *accessSecret)
+	// httpClient := config.Client(oauth1.NoContext, token)
+
+	// client := twitter.NewClient(httpClient)
+	// fmt.Println(client)
 
 	// :TODO CLI化
 	// registerAll(client)
